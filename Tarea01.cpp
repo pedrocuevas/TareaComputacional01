d1754423dfe56c7d75c8bb56de8dde5d382a7a50
Milii2 <Milii2@Milii-PC>
2013-03-28 16:36:36 -0400

    codigo comentado

--- Tarea01.cpp
+++ Tarea01.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void imprimir(char M[8][8]){
+void imprimir(char M[8][8]){ //Función imprime matriz
         int i=0;
         int j=0;
         system("clear");
@@ -15,10 +15,10 @@ void imprimir(char M[8][8]){
         }
         cout<<"\n";
         }
-  for(int z=0; z<10000000; z++){}
+  for(int z=0; z<10000000; z++){} //Ciclo de demora
 }
 
-bool prohibido(int a,int b){
+bool prohibido(int a,int b){ //Función que verifica si es una posición prohibida
     if(a==2 && b==5)
     return false;
     else if(a==5 && b==2)
@@ -39,18 +39,19 @@ int main() {
         int i=0;
         int j=0;
 
-        int x = 7;
+        int x = 7; //Posición inicial del caballo x=7,y=0
         int y = 0;
-
+        M[7][0]='X';
         int cont = 0;
 
         for(i=0;i<8;i++){
                 for(j=0;j<8;j++){
-                M[i][j]='O';
+                M[i][j]='O';    //Inicialización de la matriz
                 }
         }
         
-        M[7][0]='X';
+        
+        //Posicion de los 4 peones
         M[2][2]='O';
         M[2][5]='O';
         M[5][2]='O';
@@ -58,25 +59,22 @@ int main() {
 
         imprimir(M);
 
-        
-    
-        int k=0;
 
-        while(cont<59){
+        while(cont<59){ //Ciclo para determinar numero de casillas pintadas
  
-        int a = 1+(rand()%8);
+        int a = 1+(rand()%8); //Declaración de variable random() para seleccionar de 1 a 8 posiciones posibles
 
 
-        switch(a)
+        switch(a) //Ciclo con numeros aleatorios
         {
         case 1:
         {
-                if((x+2) < 9 && (y+1) < 9){
+                if((x+2) < 9 && (y+1) < 9){ //Condición para no salirse del tablero
        
                 if(prohibido(x+2,y+1)){              
                           x=x+2;
                           y=y+1;
-                              if(M[x][y]=='O'){
+                              if(M[x][y]=='O'){ //Si encuentra "O", pinta y suma 1 a la variable cont
                               cont++;
                               M[x][y]='X';
                               } 
