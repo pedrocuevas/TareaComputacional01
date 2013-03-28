27ffc178e30ee3a67499bd2552fbe2574f23ac7b
pedromatias.cm@gmail.com <pedro@pedro-laptop.(none)>
2013-03-28 00:41:12 -0400

    Movidas posibles del caballo

--- Tarea01.cpp
+++ Tarea01.cpp
@@ -38,6 +38,12 @@ int main() {
 char M[8][8];
 int i=0;
 int j=0;
+
+int x = 7;
+int y = 0;
+
+int cont = 0;
+
 for(i=0;i<8;i++){
 for(j=0;j<8;j++){
 M[i][j]='O';
@@ -49,6 +55,136 @@ M[5][2]='O';
 M[5][5]='O';
 
 imprimir(M);
+
+
+if((x+2) < 9 && (y+1) < 9){
+       
+if(prohibido(x+2,y+1)){              
+                          x=x+2;
+                          y=y+1;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              } 
+                                 
+                          }
+      imprimir(M);           
+       
+       }
+
+         
+if ((x+2) < 9 && (y-1) >=0){
+      
+        if(prohibido(x+2,y-1)){ 
+                          x=x+2;
+                          y=y-1;
+                              if(M[x][y]=='O' ){
+                              cont++;
+                              M[x][y]='X';
+                              }  
+                                   
+                          }
+        imprimir(M);
+      
+      }
+
+        
+if ((x-2) >= 0 && (y+1) < 9){
+    
+      if(prohibido(x-2,y+1)){
+                          x=x-2;
+                          y=y+1;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              } 
+                                             
+                                   
+                          }
+      imprimir(M);
+      
+      
+      }
+
+       
+  if ((x-2) >= 0 && (y-1) >= 0){
+      
+        if(prohibido(x-2,y-1)){ 
+                          x=x-2;
+                          y=y-1;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              }  
+                                 
+                          }
+        imprimir(M);
+      
+      
+      }
+  
+  if ((x+1) < 9 && (y+2) < 9){
+      
+        if(prohibido(x+1,y+2)){ 
+                          x=x+1;
+                          y=y+2;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              }
+                                
+                          }
+        imprimir(M);
+      
+      
+      }
+   
+  if ((x+1) < 9 && (y-2) >=0){
+      
+        if(prohibido(x+1,y-2)){ 
+                          x=x+1;
+                          y=y-2;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              }  
+                           
+                          }
+       imprimir(M);
+      
+      
+      }
+        
+  if ((x-1) >= 0 && (y+2) < 9){
+      
+        if(prohibido(x-1,y+2)){ 
+                          x=x-1;
+                          y=y+2;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              }  
+                                
+                          }
+        imprimir(M);
+      
+      
+      }
+       
+  if ((x-1) >= 0 && (y-2) >= 0){
+      
+        if(prohibido(x-1,y-2)){ 
+                          x=x-1;
+                          y=y-2;
+                              if(M[x][y]=='O'){
+                              cont++;
+                              M[x][y]='X';
+                              } 
+                              
+                          }
+        imprimir(M); 
+      
+      }
 
 
 }
